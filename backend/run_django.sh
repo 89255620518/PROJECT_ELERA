eb4be794a18da7f2658bd4aa7c82614cda5e3aca
root <root@gepuvfcjxp.local>
2025-05-16 21:19:42 +0000

    add file me

--- backend/run_django.sh
+++ backend/run_django.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ~/PROJECT_ELERA/backend/venv/bin/activate
+source /var/www/PROJECT_ELERA/backend/venv/bin/activate
 gunicorn --bind 0.0.0.0:8000 real_estate_app.wsgi:application 
